5776f7cb434c2116d9df94163600010d0a217863
Your Name <you@example.com>
2019-12-25 12:42:47 +0000

    Code

--- automobile/automobile/doctype/stock.py
+++ automobile/automobile/doctype/stock.py
@@ -21,7 +21,8 @@ def transferStock(doc,method):
 				"qty": d.qty,
 				"uom": d.uom,
 				"s_warehouse": doc.set_warehouse,
-				"t_warehouse": "Allotted - C"
+				"t_warehouse": "Allotted - C",
+				"serial_no": d.chassis_number
 			}
 			]
 			})
@@ -48,3 +49,69 @@ def getPrice(selling_price_list,item_code,from_date):
 				%s between valid_from and valid_upto;""",(selling_price_list,item_code,from_date),as_list=1)
 	return mt
 
+@frappe.whitelist(allow_guest=True)
+def get_data(selling_price_list,item_code,from_date):
+        invoice = frappe.db.sql("""select item_code,item_name,price_list_rate,
+					((price_list_rate * 0.28) + (price_list_rate * (gst_cess/100))),
+					ex_showroom,handling_charges,
+					IF(cc >= 1001 and cc <= 1500,((ex_showroom * 0.95) * (0.03283 * (100 - 10)) / 100),((ex_showroom * 0.95) * (0.0344 * (100 - 10)) / 100)),
+					(case
+						 when cc <= 1000 then 8240
+						 when cc >= 1001 and cc <= 1500 then 12488
+						 when cc >= 1501 then 27259
+					end),
+					(ex_showroom * 0.95) * 0.006,
+					(IF(cc >= 1001 and cc <= 1500,((ex_showroom * 0.95) * (0.03283 * (100 - 10)) / 100),((ex_showroom * 0.95) * (0.0344 * (100 - 10)) / 100)) + 
+                                        (case
+                                                 when cc <= 1000 then 8240
+                                                 when cc >= 1001 and cc <= 1500 then 12488
+                                                 when cc >= 1501 then 27259
+                                        end) + 
+                                        (ex_showroom * 0.95) * 0.006) * 0.18,
+					IF(booking_type="CORPORATE", price_list_rate * 0.12, price_list_rate * 0.06),
+					passing_charges,
+					IF(booking_type="TAXI",3350 ,2600),
+					(case
+                                            when price_list_rate > 299999 and price_list_rate < 499999 then (price_list_rate * 0.0175) + 25
+					    when price_list_rate > 499999 and price_list_rate < 999999 then (price_list_rate * 0.02) + 25
+					    when price_list_rate > 999999 and price_list_rate < 1999999 then (price_list_rate * 0.0225) + 25
+					    when price_list_rate > 2999999 then (price_list_rate * 0.025) + 25
+					    when price_list_rate < 299999 then (price_list_rate * 0.001) + 25
+                                        end),
+					ex_warrenty,rsa_1_year,basic_kit,fastag,
+					IF(ex_showroom > 1000000, ex_showroom * 0.01, 0.0),
+					(ex_showroom + handling_charges +
+					IF(cc >= 1001 and cc <= 1500,((ex_showroom * 0.95) * (0.03283 * (100 - 10)) / 100),((ex_showroom * 0.95) * (0.0344 * (100 - 10)) / 100)) +
+					(case
+						 when cc <= 1000 then 8240
+						 when cc >= 1001 and cc <= 1500 then 12488
+						 when cc >= 1501 then 27259
+					end) +
+					((ex_showroom * 0.95) * 0.006) +
+					(IF(cc >= 1001 and cc <= 1500,((ex_showroom * 0.95) * (0.03283 * (100 - 10)) / 100),((ex_showroom * 0.95) * (0.0344 * (100 - 10)) / 100)) + 
+                                        (case
+                                                 when cc <= 1000 then 8240
+                                                 when cc >= 1001 and cc <= 1500 then 12488
+                                                 when cc >= 1501 then 27259
+                                        end) + 
+                                        (ex_showroom * 0.95) * 0.006) * 0.18 +
+					IF(booking_type="CORPORATE", price_list_rate * 0.12, price_list_rate * 0.06) +
+					passing_charges +
+					IF(booking_type="TAXI",3350 ,2600) +
+					(case
+                                            when price_list_rate > 299999 and price_list_rate < 499999 then (price_list_rate * 0.0175) + 25
+					    when price_list_rate > 499999 and price_list_rate < 999999 then (price_list_rate * 0.02) + 25
+					    when price_list_rate > 999999 and price_list_rate < 1999999 then (price_list_rate * 0.0225) + 25
+					    when price_list_rate > 2999999 then (price_list_rate * 0.025) + 25
+					    when price_list_rate < 299999 then (price_list_rate * 0.001) + 25
+                                        end) +
+					ex_warrenty + rsa_1_year + basic_kit + fastag +
+					IF(ex_showroom > 1000000, ex_showroom * 0.01, 0.0)
+					),gst_cess_accoun,gst_cess,
+					sgst_14_account, sgst_14,
+					cgst_14_account, cgst_14,
+					sgst_9_account, sgst_9,
+					cgst_9_account, cgst_9
+				from `tabItem Price` where selling = 1 and price_list = %s and item_code = %s and 
+                                %s between valid_from and valid_upto;""",(selling_price_list,item_code,from_date),as_list=1)
+        return invoice
