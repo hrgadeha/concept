799027f8813f3e566da05085b294efb71623e88c
Your Name <you@example.com>
2020-03-12 13:36:31 +0000

    Commit 12/03

--- automobile/automobile/doctype/automobile_settings/automobile_settings.js
+++ automobile/automobile/doctype/automobile_settings/automobile_settings.js
@@ -6,3 +6,23 @@ frappe.ui.form.on('Automobile Settings', {
 
 	// }
 });
+
+frappe.ui.form.on('Automobile Settings', {
+	insert_standard_service_item(frm) {
+	    if(frm.doc.item_inserted === 0){
+    frappe.call({
+        "method": "automobile.automobile.doctype.automobile_settings.automobile_settings.createItem",
+        args: {
+        },
+        callback:function(r){
+            frm.set_value("item_inserted",1);
+            frm.save();
+    }
+});
+}
+    if(frm.doc.item_inserted == 1){
+        msgprint("Service Item Already Added");
+    }
+}
+});
+
--- automobile/automobile/doctype/automobile_settings/automobile_settings.py
+++ automobile/automobile/doctype/automobile_settings/automobile_settings.py
@@ -3,8 +3,155 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import msgprint
 from frappe.model.document import Document
 
 class AutomobileSettings(Document):
 	pass
+
+@frappe.whitelist(allow_guest=True)
+def createItem():
+	item = frappe.get_doc({
+	"doctype": "Item",
+	"item_code": "CRTM Charges",
+	"item_name": "CRTM Charges",
+	"item_group": "Services",
+	"stock_uom": "Nos",
+	"is_sales_item": 1,
+	"description": "CRTM Charges"
+	})
+	item.insert()
+	item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "Extended Warranty",
+        "item_name": "Extended Warranty",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "Extended Warranty"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "INSURANCE GST",
+        "item_name": "INSURANCE GST",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "INSURANCE GST"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "INSURANCE Own Damage",
+        "item_name": "INSURANCE Own Damage",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "INSURANCE Own Damage"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "INSURANCE TP & Others",
+        "item_name": "INSURANCE TP & Others",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "INSURANCE TP & Others"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "INSURANCE Zero Dep",
+        "item_name": "INSURANCE Zero Dep",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "INSURANCE Zero Dep"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "M. Tax",
+        "item_name": "M. Tax",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "M. Tax"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "Passing Charges",
+        "item_name": "Passing Charges",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "Passing Charges"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "REGI. CHARGES",
+        "item_name": "REGI. CHARGES",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "REGI. CHARGES"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "RTO Tax",
+        "item_name": "RTO Tax",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "RTO Tax"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "RSA CHARGES (1 YEAR)",
+        "item_name": "RSA CHARGES (1 YEAR)",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "RSA CHARGES (1 YEAR)"
+        })
+        item.insert()
+        item.save()
+
+	item = frappe.get_doc({
+        "doctype": "Item",
+        "item_code": "Handling Charges",
+        "item_name": "Handling Charges",
+        "item_group": "Services",
+        "stock_uom": "Nos",
+        "is_sales_item": 1,
+        "description": "Handling Charges"
+        })
+        item.insert()
+        item.save()
